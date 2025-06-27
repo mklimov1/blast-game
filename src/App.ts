fcda46df0c3ca0077e43010207923a754c513495
Maksim Klimov <maxklimov13@yandex.ru>
2025-06-27 12:14:13 +0200

    add App

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,16 @@
-export class App {
-  constructor() {
+import { Application } from "pixi.js";
+import AssetsLoader from "./AssetsLoader";
 
+export default class App {
+  private app = new Application();
+
+  public async init() {
+    await this.app.init();
+    await AssetsLoader.load();
+  }
+
+  public render(node: HTMLElement) {
+    node.appendChild(this.app.canvas);
+    this.app.resizeTo = node;
   }
 }
--- src/AssetsLoader.ts
+++ src/AssetsLoader.ts
@@ -1,7 +1,22 @@
-export class AssetsLoader {
-  loaded = false;
+import { Assets } from "pixi.js";
+import fieldImg from './assets/field.png';
 
-  static load() {
+const textures = {
+  field: {
+    alias: "field",
+    src: fieldImg
+  }
+};
 
+export default class AssetsLoader {
+  private static loaded = false;
+
+  static async load() {
+    if (AssetsLoader.loaded) return;
+
+    await Assets.addBundle("main", textures);
+    await Assets.loadBundle("main");
+
+    AssetsLoader.loaded = true;
   }
 }
