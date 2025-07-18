a122001f86308017eb9d41e22720806fcad58922
Maksim Klimov <maxklimov13@yandex.ru>
2025-07-18 23:37:17 +0200

    refactor: AssetsLoader

--- src/AssetsLoader.ts
+++ src/AssetsLoader.ts
@@ -4,14 +4,25 @@ import { Bundles } from "./generated";
 import manifest from './generated/manifest.json';
 
 export default class AssetsLoader {
-  private static loaded = false;
+  private static bundles: Record<string, boolean>;
 
-  static async load() {
-    if (AssetsLoader.loaded) return;
+  private static inited = false;
 
+  static async init() {
+    if (this.inited) return;
     await Assets.init({ manifest, basePath: 'assets/' });
-    await Assets.loadBundle(Bundles.GAME);
+    this.bundles = Object.keys(Bundles).reduce((prev, bundleName) => ({
+      ...prev,
+      [bundleName]: false,
+    }), {});
+    this.inited = true;
+  }
 
-    AssetsLoader.loaded = true;
+  static async load(bundle: keyof typeof Bundles) {
+    if (!this.inited) return;
+    const bundleName = Bundles[bundle];
+
+    await Assets.loadBundle(bundleName);
+    this.bundles[bundle] = true;
   }
 }
