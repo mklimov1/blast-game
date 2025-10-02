a93d94e6b15f2d4fd4b2eb4f3d5d06653f3794d2
Maksim Klimov <maxklimov13@yandex.ru>
2025-10-02 20:11:21 +0200

    feat: add star field animation for game over screen

--- src/shared/ui/GameOverScene.ts
+++ src/shared/ui/GameOverScene.ts
@@ -1,11 +1,11 @@
 import { Tween } from '@tweenjs/tween.js';
-import { Graphics, type Size } from 'pixi.js';
+import { type Size } from 'pixi.js';
 
 import { sceneManager } from '@/app';
-import { tweenGroup, Button, Text, AssetsLoader, appEventEmitter, Scene } from '@/shared';
+import { tweenGroup, Button, Text, AssetsLoader, appEventEmitter, Scene, StarField } from '@/shared';
 
 export class GameOverScene extends Scene {
-  private background!: Graphics;
+  private background!: StarField;
 
   protected text: string = '';
 
@@ -14,7 +14,7 @@ export class GameOverScene extends Scene {
   private button!: Button;
 
   protected create() {
-    this.background = this.createBackground();
+    this.background = new StarField();
     this.textEl = this.createText(this.text);
     this.button = new Button('MAIN MENU', 1);
 
@@ -38,19 +38,6 @@ export class GameOverScene extends Scene {
     return textObject;
   }
 
-  private createBackground() {
-    const graphics = new Graphics();
-
-    graphics
-      .rect(0, 0, 1000, 1000)
-      .fill({
-        color: '#000000',
-        alpha: 0.7,
-      });
-
-    return graphics;
-  }
-
   private animate(show: boolean, onComplete?: () => void) {
     const from = show ? 0 : 1;
     const to = show ? 1 : 0;
@@ -70,24 +57,26 @@ export class GameOverScene extends Scene {
 
   show(animate = true) {
     this.view.visible = true;
+    this.background.start();
     if (animate) {
       this.animate(true);
     }
   }
 
   hide(animate = true) {
-    if (animate) {
-      this.animate(false, () => {
-        this.view.visible = false;
-      });
-    } else {
+    const cb = () => {
       this.view.visible = false;
+    };
+
+    if (animate) {
+      this.animate(false, cb);
+    } else {
+      cb();
     }
   }
 
   private resize({ width, height }: Size) {
-    this.background.width = width;
-    this.background.height = height;
+    this.background.resize({ width, height });
 
     this.textEl.position.set(
       width * 0.5,
@@ -100,6 +89,7 @@ export class GameOverScene extends Scene {
   }
 
   finishScene() {
+    this.background.stop();
     super.finishScene();
     sceneManager.changeScene('mainMenu');
   }
