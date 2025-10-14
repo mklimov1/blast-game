08c00b7a6bb3c4827c29a9cc58424988b07ce853
Maxim Klimov <maxklimov13@yandex.ru>
2025-10-14 12:53:46 +0200

    refactor: scoreboard

--- src/widgets/game-stats/ui/GameStats.ts
+++ src/widgets/game-stats/ui/GameStats.ts
@@ -1,29 +1,20 @@
-import { Container, Sprite, type Size } from 'pixi.js';
-
 import { Mode, type TProgress, blastGameStore } from '@/pages';
 import { Text } from '@/shared';
+import { Scoreboard } from '@/shared/ui/Scoreboard';
 
-export class GameStats extends Container {
-  private frame = Sprite.from('ui/panel_score');
-
+export class GameStats extends Scoreboard {
   private attemptsLeftText = this.createAttemptsLeftText();
 
   private scoreLabel = this.createScoreLabel();
 
   private scoreValueText = this.createScoreValueText();
 
-  private defaultSize: Size = {
-    width: this.frame.width,
-    height: this.frame.height,
-  };
-
   constructor(attempts: number) {
     super();
-    this.pivot.set(this.defaultSize.width, this.defaultSize.height * 0.5);
 
     this.attemptsLeftText.text = attempts;
 
-    this.addChild(this.frame, this.attemptsLeftText, this.scoreLabel, this.scoreValueText);
+    this.addChild(this.attemptsLeftText, this.scoreLabel, this.scoreValueText);
     this.subscribeEvents();
   }
 
@@ -60,15 +51,6 @@ export class GameStats extends Container {
     text.y = this.frame.height * 0.79;
 
     return text;
-  }
-
-  public resize({ width, height }: Size) {
-    const scale = Math.min(
-      width * 0.4 / this.defaultSize.width,
-      height * 0.35 / this.defaultSize.height,
-    );
-    this.position.set(width * 0.95, height * 0.5);
-    this.scale.set(scale);
   }
 
   private updateStats(payload: TProgress) {
