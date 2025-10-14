eb91973e7e2eb53aa1ea368a8b0300fda4581958
Maksim Klimov <maxklimov13@yandex.ru>
2025-10-14 23:01:28 +0200

    Revert "chore: remove @eslint/js"

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import pluginImport from 'eslint-plugin-import';
 import globals from 'globals';
@@ -8,13 +9,21 @@ export default defineConfig([
     ignores: ['node_modules/**', 'dist/**', 'src/shared/generated/**', '.assetpack/**'],
   },
   {
-    files: ['**/*.{ts,mts,cts}'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: {
       '@typescript-eslint': tseslint.plugin,
       import: pluginImport,
+      js,
     },
-    ...tseslint.configs.recommended,
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: { globals: globals.browser },
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     rules: {
       'semi': ['error', 'always'],
       'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
@@ -25,32 +34,54 @@ export default defineConfig([
       'object-curly-spacing': ['error', 'always'],
       'no-console': ['warn', { allow: ['warn', 'error'] }],
       'padding-line-between-statements': [
-        { blankLine: 'always', prev: 'return', next: '*' },
-        { blankLine: 'any', prev: 'block-like', next: '*' },
-        { blankLine: 'any', prev: '*', next: 'block-like' },
+        'error',
+        { 'blankLine': 'always', 'prev': 'return', 'next': '*' },
+        { 'blankLine': 'any', 'prev': 'block-like', 'next': '*' },
+        { 'blankLine': 'any', 'prev': '*', 'next': 'block-like' },
       ],
-      'key-spacing': [
-        2,
-        {
-          singleLine: { beforeColon: false, afterColon: true },
-          multiLine: { beforeColon: false, afterColon: true },
+      'key-spacing': [2, {
+        'singleLine': {
+          'beforeColon': false,
+          'afterColon': true,
         },
-      ],
+        'multiLine': {
+          'beforeColon': false,
+          'afterColon': true,
+        },
+      }],
+
       'import/order': ['error', {
-        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'type'],
-        pathGroups: [{ pattern: '@/**', group: 'internal' }],
-        pathGroupsExcludedImportTypes: ['builtin'],
+        'groups': [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+        ],
+        'pathGroups': [
+          {
+            pattern: '@/**',
+            group: 'internal',
+          },
+        ],
+        'pathGroupsExcludedImportTypes': ['builtin'],
         'newlines-between': 'always',
-        alphabetize: { order: 'asc', caseInsensitive: true },
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true,
+        },
       }],
+
       'max-len': ['error', {
-        code: 100,
-        tabWidth: 2,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
+        'code': 100,
+        'tabWidth': 2,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreRegExpLiterals': true,
       }],
+
       'prefer-template': 'error',
       'template-curly-spacing': ['error', 'never'],
       'quotes': ['error', 'single'],
